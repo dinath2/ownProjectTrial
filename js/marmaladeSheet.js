6569bf7efd1dcd464005d5b6d4614a9654c50d0b
dinath2 <65164585+dinath2@users.noreply.github.com>
2020-09-07 14:26:37 +0200

    yup

---
+++ js/marmaladeSheet.js
@@ -0,0 +1,13 @@
+function sheetView() {
+
+    const character = model.character;
+    const stats = model.stats;
+
+    contentDiv.innerHTML = `
+    <div id="sheetContent">
+
+        <div id="infoGrid"></div>
+        
+    </div>;`
+
+}
